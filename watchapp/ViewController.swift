4d5f98e294f86d0ee5ec84535b1c0b0820dfcfca
Mathinho <matt2nd@gmx.net>
2019-04-11 22:48:09 +0200

    Initial Commit

---
+++ watchapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchapp
+//
+//  Created by mathinho on 11.04.19.
+//  Copyright © 2019 mathinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
